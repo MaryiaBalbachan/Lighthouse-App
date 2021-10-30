271474a468cc23c9995b8c4334521e858f9a4853
Maryia Balbachan <59488833+MaryiaBalbachan@users.noreply.github.com>
2021-10-30 20:56:45 +0100

    LighthouseList/Card/Recycler view (adaptor not working)

--- app/src/main/java/org/wit/lighthouse/activities/LighthouseListActivity.kt
+++ app/src/main/java/org/wit/lighthouse/activities/LighthouseListActivity.kt
@@ -2,11 +2,56 @@ package org.wit.lighthouse.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import org.wit.lighthouse.R
+import org.wit.lighthouse.databinding.ActivityLighthouseListBinding
+import org.wit.lighthouse.databinding.CardLighthouseBinding
+import org.wit.lighthouse.main.MainApp
+import org.wit.lighthouse.models.LighthouseModel
 
 class LighthouseListActivity : AppCompatActivity() {
+
+    lateinit var app: MainApp
+    private lateinit var binding: ActivityLighthouseListBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityLighthouseListBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_lighthouse_list)
+
+        app = application as MainApp
+
+        val layoutManager = LinearLayoutManager(this)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = LighthouseAdapter(app.lighthouses)
+    }
+}
+class LighthouseAdapter constructor(private var lighthouses: List<LighthouseModel>) :
+    RecyclerView.Adapter<LighthouseAdapter.MainHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainHolder {
+        val binding = CardLighthouseBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+
+        return MainHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MainHolder, position: Int) {
+        val lighthouse = lighthouses[holder.adapterPosition]
+        holder.bind(lighthouse)
+    }
+
+    override fun getItemCount(): Int = lighthouses.size
+
+    class MainHolder(private val binding : CardLighthouseBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(placemark: LighthouseModel) {
+            binding.lighthouseTitle.text = placemark.title
+            binding.description.text = placemark.description
+        }
     }
 }
--- app/src/main/java/org/wit/lighthouse/main/MainApp.kt
+++ app/src/main/java/org/wit/lighthouse/main/MainApp.kt
@@ -6,9 +6,13 @@ import timber.log.Timber.i
 
 class MainApp : Application() {
     val lighthouses = ArrayList<LighthouseModel>()
+
     override fun onCreate() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         i("Lighthouse started")
+        lighthouses.add(LighthouseModel("One", "About one..."))
+        lighthouses.add(LighthouseModel("Two", "About one..."))
+        lighthouses.add(LighthouseModel("Three", "About one..."))
     }
 }
