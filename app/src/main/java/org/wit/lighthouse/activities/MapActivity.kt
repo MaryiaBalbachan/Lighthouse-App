a3dde59367df535982116b294c35f5efd863e90c
Maryia Balbachan <59488833+MaryiaBalbachan@users.noreply.github.com>
2021-10-31 14:31:21 +0000

    Location changed to Hook Lighthouse

--- app/src/main/java/org/wit/lighthouse/activities/MapActivity.kt
+++ app/src/main/java/org/wit/lighthouse/activities/MapActivity.kt
@@ -1,8 +1,6 @@
 package org.wit.lighthouse.activities
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
@@ -10,8 +8,9 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import org.wit.lighthouse.R
-//import org.wit.lighthouse.activities.databinding.ActivityMapBinding
 import org.wit.lighthouse.databinding.ActivityMapBinding
+
+
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -24,27 +23,17 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        val hook = LatLng(52.1238, -6.9292)
+        mMap.addMarker(MarkerOptions().position(hook).title("Marker at Hook Lighthouse"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(hook, 16f))
     }
 }
